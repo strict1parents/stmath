aa3e1c0fdc44cec5d6a809dd4fdff11b62d2ba95
strict1parents <hastthis@gmail.com>
2025-04-10 11:43:39 +0200

    added header. Closes #2

---
+++ include/stmath.h
@@ -0,0 +1,17 @@
+#ifndef STMATH_H
+#define STMATH_H
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+
+double st_pow(double base, int exp);
+double st_sqrt(double x);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
