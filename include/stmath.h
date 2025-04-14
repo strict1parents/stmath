8abce95d4b4952cf90332c071b24a94f11e1bd6e
strict1parents <hastthis@gmail.com>
2025-04-14 22:07:23 +0200

    closed issue #6. Closes #6.

--- include/stmath.h
+++ include/stmath.h
@@ -11,8 +11,14 @@
 //---
 #ifdef ST_MATH_USE_FLOAT
   typedef float st_real;
+  #define st_e            2.7182818f
+  #define st_pi           3.1415926f
+  #define st_epsilon      1e-5f
 #else 
   typedef double st_real;
+  #define st_e            2.718281828459045
+  #define st_pi           3.141592653589793
+  #define st_epsilon      1e-10
 #endif
 
 
