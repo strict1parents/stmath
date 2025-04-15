262ead94375ff0bfddebedfaf609aa16a77d7d3b
strict1parents <hastthis@gmail.com>
2025-04-15 22:15:46 +0200

    added consts.h

---
+++ include/stmath/consts.h
@@ -0,0 +1,16 @@
+#ifndef ST_MATH_CONSTS_H
+#define ST_MATH_CONSTS_H
+
+
+#ifdef ST_MATH_USE_FLOAT
+  #define st_e          2.7182818f
+  #define st_pi         3.1415926f
+  #define st_epsilon    1e-5f
+#else 
+  #define st_e          2.718281828459045
+  #define st_pi         3.141592653589793
+  #define st_epsilon    1e-10
+#endif
+
+
+#endif
