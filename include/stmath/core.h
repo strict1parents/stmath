cd4e466aa7a7403e81621cc3dd876ba51c3459dd
strict1parents <hastthis@gmail.com>
2025-04-17 01:41:09 +0200

    added inline. Closes #17

--- include/stmath/core.h
+++ include/stmath/core.h
@@ -1,6 +1,10 @@
 #ifndef STMATH_CORE_H
 #define STMATH_CORE_H
 
+
+#include "consts.h"
+
+
 #ifdef __cplusplus
   extern "C" {
 #endif
@@ -14,15 +18,36 @@
 typedef unsigned int uint32_t
 
 
-st_real      st_pow(st_real base, int exp);
-st_real      st_sqrt(st_real x);
-st_real      st_abs(st_real a);
-st_real      st_min(st_real a, st_real b);
-st_real      st_max(st_real a, st_real b);
-int          st_fact(uint32_t x);
-int          st_sign(st_real x);
-void         st_round(st_real *x);
-int          st_mod(int a,int b);
+static inline st_real st_abs(st_real a) 
+{
+  return (a<0)? -a:a;
+}
+
+static inline st_real st_min(st_real a, st_real b)
+{
+  return (a<b)? a:b;
+}
+
+static inline st_real st_max(st_real a, st_real b)
+{
+  return (a>b)? a:b;
+}
+
+static inline st_real st_sign(st_real x)
+{
+  return (x<0) -1:(x==0)? 0:1;
+}
+
+static inline int st_mod(int a, int b)
+{
+  return (b==0)? 0:(a%b);
+}
+
+
+st_real                     st_pow(st_real base, int exp);
+st_real                     st_sqrt(st_real x);
+int                         st_fact(uint32_t x);
+void                        st_round(st_real *x);
 
 
 #ifdef __cplusplus
