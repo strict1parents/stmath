f50e2ca3cd5528cb2cc67cb39776e77afdb2ea79
strict1parents <hastthis@gmail.com>
2025-04-17 14:15:19 +0200

    implemented st_exp method. Closes #18

--- src/stmath.c
+++ src/stmath.c
@@ -15,6 +15,16 @@ st_real st_pow(st_real base, int exp)
   } 
 }
 
+
+int st_sqrt_i(int x)
+{
+  if (x<=0) return 0;
+  int i=0;
+  int maxiter=100;
+  int result=x/2;
+  while (
+}
+
 st_real st_sqrt(st_real x) 
 {
   if (x <= 0) return 0;
@@ -27,22 +37,8 @@ st_real st_sqrt(st_real x)
   return result;
 }
 
-/*st_real st_abs(st_real a) 
-{
-  return (a<0)? -a:a;
-}
 
-st_real st_min(st_real a, st_real b) 
-{
-  return a<b? a:b;
-}
-
-st_real st_max(st_real a, st_real b) 
-{
-  return a>b? a:b;
-}
-*/
-int st_fact(uint32_t x)
+int st_fact_i(uint32_t x)
 {
   if (x==0) return 1;
   if x>12 return -1;
@@ -52,19 +48,24 @@ int st_fact(uint32_t x)
   }
   return result;
 }
-/*
-int st_sign(st_real x) 
+
+st_real st_fact_r(st_real x)
 {
-  return (x<0)? -1: (st_abs(x)<st_epsilon)? 0:1;
+   
 }
-*/
+
 void st_round(st_real *x)
 {
   if !x return;
   *x=(st_real)((int)(*x+(*x>=0?0.5:-0.5)));
 }
-/*
-int st_mod(int a, int b) 
+
+st_real st_exp(int x)
 {
-  return (b==0)? 0:(a%b);
-}*/
+  st_real result=1.0;
+  for (int i=0; i<x; ++i) {
+    result*=st_e; 
+  }
+  return result;
+}
+
