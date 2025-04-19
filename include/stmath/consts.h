a7fdc6a022d0a9df45156e0b7f616816cff0df21
strict1parents <hastthis@gmail.com>
2025-04-20 01:25:29 +0200

    added st_ln10

--- include/stmath/consts.h
+++ include/stmath/consts.h
@@ -8,12 +8,14 @@
   #define st_epsilon    1e-5f
   #define st_twoPI      2.0*st_pi
   #define st_ln2        0.6931472f
+  #define st_ln10       2.30258509f
 #else 
   #define st_e          2.718281828459045
   #define st_pi         3.141592653589793
   #define st_epsilon    1e-10
   #define st_twoPI      2.0*st_pi
   #define st_ln2        0.6931471805599453
+  #define st_ln10       2.30258509299404568402
 #endif
 
 #ifndef st_inf
