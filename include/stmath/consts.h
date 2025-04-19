7a8b216002872fdb94952888e3017760740c052b
strict1parents <hastthis@gmail.com>
2025-04-19 17:12:20 +0200

    solve some problems

--- include/stmath/consts.h
+++ include/stmath/consts.h
@@ -6,12 +6,12 @@
   #define st_e          2.7182818f
   #define st_pi         3.1415926f
   #define st_epsilon    1e-5f
-  #define st_twoPI      2*st_pi
+  #define st_twoPI      2.0*st_pi
 #else 
   #define st_e          2.718281828459045
   #define st_pi         3.141592653589793
   #define st_epsilon    1e-10
-  #define st_twoPI      2*st_pi
+  #define st_twoPI      2.0*st_pi
 #endif
 
 #ifndef st_inf
