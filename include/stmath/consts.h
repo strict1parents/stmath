9765d7a368c9c3b0f7cea197651b84948faa790a
strict1parents <hastthis@gmail.com>
2025-04-20 00:47:09 +0200

    added the value for the ln2

--- include/stmath/consts.h
+++ include/stmath/consts.h
@@ -7,11 +7,13 @@
   #define st_pi         3.1415926f
   #define st_epsilon    1e-5f
   #define st_twoPI      2.0*st_pi
+  #define st_ln2        0.6931472f
 #else 
   #define st_e          2.718281828459045
   #define st_pi         3.141592653589793
   #define st_epsilon    1e-10
   #define st_twoPI      2.0*st_pi
+  #define st_ln2        0.6931471805599453
 #endif
 
 #ifndef st_inf
