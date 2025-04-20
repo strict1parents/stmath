98b82b1c5dcc58c88b3d527d363460927ed04e80
strict1parents <hastthis@gmail.com>
2025-04-21 01:42:18 +0200

    declared st_frac. Closes #37

--- include/stmath/core.h
+++ include/stmath/core.h
@@ -109,6 +109,7 @@ st_real                     st_log10(st_real x);
 st_real                     st_log(int base, st_real x);
 st_real                     st_lgamma(st_real x);
 st_real                     st_clamp(st_real x, st_real min, st_real max);
+st_real                     st_frac(st_real x);
 
 
 
