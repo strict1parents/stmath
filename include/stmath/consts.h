4972606971e7966242e43c3633dacebf7600e2b3
strict1parents <hastthis@gmail.com>
2025-04-20 13:36:10 +0200

    added arrays with coeffs for Lanczos

--- include/stmath/consts.h
+++ include/stmath/consts.h
@@ -9,6 +9,16 @@
   #define st_twoPI      2.0*st_pi
   #define st_ln2        0.6931472f
   #define st_ln10       2.30258509f
+  #define G 5.0f
+  #define G_COEFFS 6
+  static const st_real g_coeffs[G_COEFFS] = {
+          1.0000000f,
+          76.1800917f,
+          -86.5053203f,
+          24.0140982f,
+          -1.2317396f,
+          0.0012086f
+        };
 #else 
   #define st_e          2.718281828459045
   #define st_pi         3.141592653589793
@@ -16,6 +26,19 @@
   #define st_twoPI      2.0*st_pi
   #define st_ln2        0.6931471805599453
   #define st_ln10       2.30258509299404568402
+  #define G 7.0
+  #define G_COEFFS 9
+  static const st_real g_coeffs[G_COEFFS] = {
+          0.99999999999980993,
+          676.5203681218851,
+          -1259.1392167224028,
+          771.32342877765313,
+          -176.61502916214059,
+          12.507343278686905,
+          -0.13857109526572012,
+          9.9843695780195716e-6,
+          1.5056327351493116e-7
+        };
 #endif
 
 #ifndef st_inf
