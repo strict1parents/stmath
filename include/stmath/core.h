7747a45129db1142fc14751993c7e0ea7fad2e29
strict1parents <hastthis@gmail.com>
2025-04-25 02:08:53 +0200

    declared st_sec. Closes #43

--- include/stmath/core.h
+++ include/stmath/core.h
@@ -126,6 +126,7 @@ st_real                     st_sin(st_real x);
 st_real                     st_cos(st_real x);
 st_real                     st_tan(st_real x);
 st_real                     st_cot(st_real x);
+st_real                     st_sec(st_real x);
 int                         st_cpsign_i(int x, int y);
 st_real                     st_cpsign_r(st_real x, st_real y);
 st_real                     st_gamma(st_real x);
