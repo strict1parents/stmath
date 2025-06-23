3a12b674f0036b4aeb3b1f68d8cb7f196e9a0309
strict1parents <hastthis@gmail.com>
2025-06-23 17:31:36 +0200

    declared st_abs_comp. Closes #58

--- include/stmath/alg.h
+++ include/stmath/alg.h
@@ -16,6 +16,7 @@ typedef struct st_complex {
 
 void							st_add_comp(struct st_complex *A, const struct st_complex *B);
 void							st_mult_comp(struct st_complex *A, const struct st_complex *B);
+st_real							st_abs_comp(struct st_complex	*A);
 
 
 #ifdef __cplusplus
